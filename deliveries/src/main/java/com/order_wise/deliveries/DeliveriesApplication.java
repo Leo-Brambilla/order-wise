38631b7dbe24ca81342e624d413938f89de49e4c
leonardo <leonardo.araujo@exago.com>
2024-11-20 16:50:48 -0300

    configuracao inicial dos microservices

---
+++ deliveries/src/main/java/com/order_wise/deliveries/DeliveriesApplication.java
@@ -0,0 +1,13 @@
+package com.order_wise.deliveries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveriesApplication.class, args);
+	}
+
+}
---
+++ order_Items/src/main/java/com/order_wise/order_Items/OrderItemsApplication.java
@@ -0,0 +1,13 @@
+package com.order_wise.order_Items;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderItemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderItemsApplication.class, args);
+	}
+
+}
